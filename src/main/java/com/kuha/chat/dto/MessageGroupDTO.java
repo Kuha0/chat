0b9c50c1c133d81a52dd9b6f0a1db7c31e669ae2
Kuha0 <arantesjos9057@yahoo.com>
2025-04-01 14:05:37 -0300

    first commit

---
+++ src/main/java/com/kuha/chat/dto/MessageGroupDTO.java
@@ -0,0 +1,9 @@
+package com.kuha.chat.dto;
+
+public class MessageGroupDTO extends MessageDTO{
+    private Integer groupId;
+
+    public Integer getGroupId(){return groupId;}
+
+    public void setGroupId(Integer groupId){this.groupId=groupId;}
+}
